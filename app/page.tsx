a3ebccf86af16561c21bd9f51ab8d2e9318f5fb9
Abhay-0103 <ab0321054@gmail.com>
2025-11-17 13:16:05 +0530

    Completed First sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <div>Welcome To Next.js 16!</div>
+        <section>
+            <h1  className="text-center">The Hub For Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All In One Place</p>
+        </section>
     )
 }
 export default Page
